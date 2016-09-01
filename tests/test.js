4f06e3a80cd080bcb3d4a9c6726c05e9fda85213
Grégoire Weber <gregoire@clevertoday.com>
2016-09-01 18:17:36 -0400

    Test cleanup

--- tests/test.js
+++ tests/test.js
@@ -1,59 +1,71 @@
 /*global describe, it, beforeEach, before, afterEach */
-var chai = require('chai'),
-  expect = chai.expect,
-  nock = require('nock'),
-  fs = require('fs'),
-  Barracks = require('../src/index.js'),
-  testDir = __dirname,
-  baseURL = 'https://domain.name',
-  mockFilePath = testDir + '/fixtures/validApplication',
-  corruptMockFilePath = testDir + '/fixtures/invalidApplication',
-  nockHeaders = {
-    'Authorization': 'validKey',
-    'Content-type': 'application/json'
-  },
-  nockHeaders_dl = {
-    'Authorization': 'validKey',
-  },
-  currentVersionId = 'v0.0.1',
-  updateVersionId = 'v0.0.2',
-  unitId = 'unit1';
+var chai = require('chai');
+var expect = chai.expect;
+var nock = require('nock');
+var fs = require('fs');
+var Barracks = require('../src/index.js');
+
+var TEST_DIRECTORY            = __dirname;
+var BASE_URL                  = 'https://domain.name';
+var CHECK_UPDATE_ENDPOINT     = '/api/device/update/check';
+var DOWNLOAD_UPDATE_ENDPOINT  = '/api/device/update/download/';
+var CORRUPT_MOCK_FILE_PATH    = TEST_DIRECTORY + '/fixtures/invalidApplication';
+var MOCK_FILE_PATH            = TEST_DIRECTORY + '/fixtures/validApplication';
+var MOCK_FILE_MD5_HASH        = '1f1133ee77f0b3c66c948ae376d55715';
+var MOCK_FILE_SIZE            = 1447;
+var CURRENT_VERSION_ID        = 'v0.0.1';
+var UPDATE_VERSION_ID         = 'v0.0.2';
+var UPDATE_ID                 = '1152723d-a267-4cd5-aaac-511e568d4681';
+var UNIT_ID                   = 'unit1';
+var NOCK_HEADERS = {
+  'Authorization': 'validKey',
+  'Content-type': 'application/json'
+};
+var NOCK_CDN_HEADERS = {
+  'Authorization': 'validKey'
+};
+
 
 beforeEach(function() {
-  mockedServer = nock(baseURL, { reqheaders: nockHeaders });
-  mockedCDNServer = nock(baseURL, { reqheaders: nockHeaders_dl });
+  mockedServer    = nock(BASE_URL, { reqheaders: NOCK_HEADERS });
+  mockedCDNServer = nock(BASE_URL, { reqheaders: NOCK_CDN_HEADERS });
+
   barracks = new Barracks({
-    baseURL: baseURL,
-    apiKey: nockHeaders.Authorization,
-    unitId: unitId,
-    downloadDir: '/tmp/files'
+    baseURL:          BASE_URL,
+    apiKey:           NOCK_HEADERS.Authorization,
+    unitId:           UNIT_ID,
+    downloadFilePath: '/tmp/myUpdate.tmp'
   });
+
+  updateProperties = { jsonkey: 'value' };
+  packageInfo = {
+    url:  BASE_URL + DOWNLOAD_UPDATE_ENDPOINT + UPDATE_ID,
+    md5:  MOCK_FILE_MD5_HASH,
+    size: MOCK_FILE_SIZE
+  };
 });
+
+function getCheckUpdateEntrypoint(customClientData) {
+  return mockedServer.post(CHECK_UPDATE_ENDPOINT, {
+    unitId:           UNIT_ID,
+    versionId:        CURRENT_VERSION_ID,
+    customClientData: customClientData
+  })
+}
 
 describe('Check for an update : ', function () {
 
   it('Should return an update when one is available', function (done) {
-    var updateProperties = { jsonkey: 'value' };
-    var packageInfo = {
-      url: baseURL+ 'update/download/1152723d-a267-4cd5-aaac-511e568d4681',
-      md5: '5f396472788fde9b770bffb7ae2c6deb',
-      size: 1447
-    };
-
-    mockedServer.post('/api/device/update/check', {
-      unitId: unitId,
-      versionId: currentVersionId,
-      customClientData: undefined
-    }).reply(200, {
-      versionId: updateVersionId,
-      packageInfo: packageInfo,
-      properties: updateProperties
+    getCheckUpdateEntrypoint().reply(200, {
+      versionId:    UPDATE_VERSION_ID,
+      packageInfo:  packageInfo,
+      properties:   updateProperties
     });
 
-    barracks.checkUpdate(currentVersionId).then(function (update) {
+    barracks.checkUpdate(CURRENT_VERSION_ID).then(function (update) {
       expect(update).to.be.a('object');
       expect(update).to.have.property('versionId');
-      expect(update.versionId).to.equals(updateVersionId);
+      expect(update.versionId).to.equals(UPDATE_VERSION_ID);
 
       expect(update).to.have.property('properties');
       expect(update.properties).to.be.a('object');
@@ -73,13 +85,9 @@ describe('Check for an update : ', function () {
 
 
   it('Should not return an update when there is none', function(done) {
-    mockedServer.post('/api/device/update/check', {
-      unitId: unitId,
-      versionId: currentVersionId,
-      customClientData: undefined
-    }).reply(204);
+    getCheckUpdateEntrypoint().reply(204);
 
-    barracks.checkUpdate(currentVersionId).then(function(update) {
+    barracks.checkUpdate(CURRENT_VERSION_ID).then(function(update) {
       expect(update).to.be.undefined;
       done();
     }).catch(function (err) {
@@ -89,28 +97,17 @@ describe('Check for an update : ', function () {
 
 
   it('Should download the update when the file is valid', function (done) {
-    var properties = { jsonkey: 'value' };
-    var packageInfo = {
-      url: baseURL + '/cdn/filename',
-      md5: '1f1133ee77f0b3c66c948ae376d55715',
-      size: 1447
-    };
-
-    mockedServer.post('/api/device/update/check', {
-      unitId: unitId,
-      versionId: currentVersionId,
-      customClientData: undefined
-    }).reply(200, {
-      versionId: updateVersionId,
-      packageInfo: packageInfo,
-      properties: properties
+    getCheckUpdateEntrypoint().reply(200, {
+      versionId:    UPDATE_VERSION_ID,
+      packageInfo:  packageInfo,
+      properties:   updateProperties
     });
 
-    mockedCDNServer.get('/cdn/filename').replyWithFile(200, mockFilePath);
+    mockedCDNServer.get(DOWNLOAD_UPDATE_ENDPOINT + UPDATE_ID).replyWithFile(200, MOCK_FILE_PATH);
 
-    barracks.checkUpdate(currentVersionId).then(function(update){
+    barracks.checkUpdate(CURRENT_VERSION_ID).then(function(update){
       update.download(update.file).then(function(file){
-        var mockFileContent = fs.readFileSync(mockFilePath).toString();
+        var mockFileContent = fs.readFileSync(MOCK_FILE_PATH).toString();
         var downloadedFileContent = fs.readFileSync(file).toString();
         expect(downloadedFileContent).to.equal(mockFileContent);
         done();
@@ -124,27 +121,16 @@ describe('Check for an update : ', function () {
 
 
   it('Should check and download the update simultaneously', function (done) {
-    var properties = { jsonkey: 'value' };
-    var packageInfo = {
-      url: baseURL + '/cdn/filename',
-      md5: '1f1133ee77f0b3c66c948ae376d55715',
-      size: 1447
-    };
-
-    mockedServer.post('/api/device/update/check', {
-      unitId: unitId,
-      versionId: currentVersionId,
-      customClientData: undefined
-    }).reply(200, {
-      versionId: updateVersionId,
-      packageInfo: packageInfo,
-      properties: properties
+    getCheckUpdateEntrypoint().reply(200, {
+      versionId:    UPDATE_VERSION_ID,
+      packageInfo:  packageInfo,
+      properties:   updateProperties
     });
 
-    mockedCDNServer.get('/cdn/filename').replyWithFile(200, mockFilePath);
+    mockedCDNServer.get(DOWNLOAD_UPDATE_ENDPOINT + UPDATE_ID).replyWithFile(200, MOCK_FILE_PATH);
 
-    barracks.checkUpdateAndDownload(currentVersionId).then(function (file) {
-      var mockFileContent = fs.readFileSync(mockFilePath).toString();
+    barracks.checkUpdateAndDownload(CURRENT_VERSION_ID).then(function (file) {
+      var mockFileContent = fs.readFileSync(MOCK_FILE_PATH).toString();
       var downloadedFileContent = fs.readFileSync(file).toString();
       expect(downloadedFileContent).to.equal(mockFileContent);
       done();
@@ -154,32 +140,22 @@ describe('Check for an update : ', function () {
   });
 
 
-  it('Should delete the update when the file is corrupted', function (done) {
-    var properties = { jsonkey: 'value' };
-    var packageInfo = {
-      url: baseURL + '/cdn/filename',
-      md5: '1f1133ee77f0b3c66c948ae376d55715',
-      size: 1447
-    };
-    
-    mockedServer.post('/api/device/update/check', {
-      unitId: unitId,
-      versionId: currentVersionId,
-      customClientData: undefined
-    }).reply(200, {
-      versionId: updateVersionId,
-      packageInfo: packageInfo,
-      properties: properties
+  it('Should delete the update when the file is corrupted', function (done) {    
+    getCheckUpdateEntrypoint().reply(200, {
+      versionId:    UPDATE_VERSION_ID,
+      packageInfo:  packageInfo,
+      properties:   updateProperties
     });
 
-    mockedCDNServer.get('/cdn/filename').replyWithFile(200, corruptMockFilePath);
+    mockedCDNServer.get(DOWNLOAD_UPDATE_ENDPOINT + UPDATE_ID).replyWithFile(200, CORRUPT_MOCK_FILE_PATH);
 
-    barracks.checkUpdate(currentVersionId).then(function(update){
+    barracks.checkUpdate(CURRENT_VERSION_ID).then(function(update){
       update.download(update.file).then(function(file){
         done('MD5 should not match');
       }).catch(function (err) {
         expect(err).to.not.equal(undefined);
-        fs.exists(barracks.options.downloadDir + '/' + updateVersionId, function (exists) {
+        expect(barracks.options.downloadFilePath).to.be.a('string');
+        fs.exists(barracks.options.downloadFilePath, function (exists) {
           expect(exists).to.equal(false);
           done();
         });
@@ -191,16 +167,11 @@ describe('Check for an update : ', function () {
 
 
   it('Should fail when the server responds with error', function (done) {
-    mockedServer.post('/api/device/update/check', {
-      unitId: unitId,
-      versionId: currentVersionId,
-      customClientData: undefined
-    })
-    .reply(400, {
+    getCheckUpdateEntrypoint().reply(400, {
       body: 'Error message'
     });
 
-    barracks.checkUpdate(currentVersionId).then(function(update){
+    barracks.checkUpdate(CURRENT_VERSION_ID).then(function(update){
       done('Should be an error');
     }).catch(function (err) {
       done();
@@ -209,28 +180,18 @@ describe('Check for an update : ', function () {
 
 
   it('Should build the correct request with custom client data', function (done) {
-    var properties = { jsonkey: 'value' };
-    var packageInfo = {
-      url: baseURL + 'update/download/1152723d-a267-4cd5-aaac-511e568d4681',
-      md5: '5f396472788fde9b770bffb7ae2c6deb',
-      size: 1447
-    };
     var customClientData = {
       customData1: 'custom value 1',
       customData2: 'custom value 2'
     }
 
-    mockedServer.post('/api/device/update/check', {
-      unitId: unitId,
-      versionId: currentVersionId,
-      customClientData: customClientData
-    }).reply(200, {
-      versionId: updateVersionId,
-      packageInfo: packageInfo,
-      properties: properties
+    getCheckUpdateEntrypoint(customClientData).reply(200, {
+      versionId:    UPDATE_VERSION_ID,
+      packageInfo:  packageInfo,
+      properties:   updateProperties
     });
 
-    barracks.checkUpdate(currentVersionId, customClientData).then(function (update) {
+    barracks.checkUpdate(CURRENT_VERSION_ID, customClientData).then(function (update) {
       done();
     }).catch(function (err) {
       done(err);
@@ -238,4 +199,3 @@ describe('Check for an update : ', function () {
 
   });
 });
-
