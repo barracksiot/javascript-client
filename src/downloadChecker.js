9e802242463e0812a74be68329cf6f01915d3f59
Grégoire Weber <gregoire@clevertoday.com>
2016-09-08 15:33:03 -0400

    Delete update package when the md5 checksum fail to be created

--- src/downloadChecker.js
+++ src/downloadChecker.js
@@ -5,6 +5,17 @@ var ERROR_DELETE_FILE_FAILED            = 'DELETE_FILE_FAILED';
 var ERROR_CHECKSUM_VERIFICATION_FAILED  = 'CHECKSUM_VERIFICATION_FAILED';
 var ERROR_MD5_HASH_CREATION_FAILED      = 'MD5_HASH_CREATION_FAILED';
 
+function deleteCorruptedFile(file, reject) {
+  fs.unlink(file, function (err) {
+    if (err) {
+      reject({
+        type: ERROR_DELETE_FILE_FAILED,
+        message: 'Error when removing file ' + file + ': ' + err
+      });
+    }
+  });
+}
+
 module.exports = {
   check: function (file, validSum) {
     return new Promise(function (resolve, reject) {
@@ -12,20 +23,14 @@ module.exports = {
         if (hash == validSum) {
           resolve();
         } else {
-          fs.unlink(file, function (err) {
-            if (err) {
-              reject({
-                type: ERROR_DELETE_FILE_FAILED,
-                message: 'Error when removing file ' + file + ': ' + err
-              });
-            }
-          });
+          deleteCorruptedFile(file, reject);
           reject({
             type: ERROR_CHECKSUM_VERIFICATION_FAILED,
             message: 'Checksum don\'t match'
           });
         }
       }).catch(function (err) {
+        deleteCorruptedFile(file, reject);
         reject({
           type: ERROR_MD5_HASH_CREATION_FAILED,
           message: err
